69810180959e2eb608514093e92061fe86203134
BrAvalos <b.r-avalos@hotmail.com>
2020-05-18 09:21:06 -0500

    Mi primer Comid hola

--- GestorProyectoGitHub/src/gestorproyectogithub/GestorProyectoGitHub.java
+++ GestorProyectoGitHub/src/gestorproyectogithub/GestorProyectoGitHub.java
@@ -13,9 +13,10 @@ public class GestorProyectoGitHub {
 
     /**
      * @param args the command line arguments
-     */
+     */ 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
